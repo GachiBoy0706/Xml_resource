5617c1ba34c798a9263ad42c413d5c584d7b004d
GachiBoy0706 <sazhin.dan@gmail.com>
2023-11-05 17:32:14 +0300

    main completed

--- main.cpp
+++ main.cpp
@@ -22,15 +22,21 @@ int main() {
 
 	std::cout << (it1 == it2) << std::endl;
 	std::cout << std::endl;
-	Xml_resource::iterator it_child = tree->add("CHILD_ADDED", 6666, it2);
-	std::cout << it_child->value << std::endl;
-	std::cout << std::endl;
-	tree->print();
-	std::cout << std::endl;
-	tree->upload("file_output.txt");
-	std::cout << std::endl;
-	std::cout << tree->erase(it_child) << std::endl;
-	tree->print();
-
+	try
+	{
+		Xml_resource::iterator it_child = tree->add("CHILD_ADDED", 6666, it2);
+		std::cout << it_child->value << std::endl;
+		std::cout << std::endl;
+		tree->print();
+		std::cout << std::endl;
+		tree->upload("file_output.txt");
+		std::cout << std::endl;
+		std::cout << tree->erase(it_child) << std::endl;
+		tree->print();
+	}
+	catch (const char* error_message)
+	{
+		std::cout << error_message << std::endl;
+	}
 	return 0;
 }
